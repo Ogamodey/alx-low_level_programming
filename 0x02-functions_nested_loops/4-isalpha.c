cc66e505a9df2b46f03956eaaa6d94fd72c82646
ogamodey <ogamodeymoses@gmail.com>
2022-12-14 10:34:02 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - checks for char alphabet
  * @c: the char to be checked
  *
- * Return: 1 if char is letter lower or upper, otherwise 0
+ * Return: 1 if char is letter lower or upper, 0 otherwise
  */
 
 int _isalpha(int c)
@@ -12,5 +12,5 @@ int _isalpha(int c)
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
 		return (1);
 	else
-		return (0)
+		return (0);
 }
