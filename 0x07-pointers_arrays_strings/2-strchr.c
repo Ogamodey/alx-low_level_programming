022f73b1180eccb0a70132168227111165f6563b
ogamodey <ogamodeymoses@gmail.com>
2023-01-03 17:32:41 -0800

    task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,23 @@
 #include "main.h"
+#define NULL 0
+
 /**
-* set_string - set value of pointer to a char
-* @s: variable of type pointer
-* @to: char
+* _strchr - locate 1st occurrence of char in string and returns pointer there
+* @s: string to search
+* @c: target characer
+* Return: pointer to that character in string
 */
 
-void set_string(char **s, char *to)
+char *_strchr(char *s, char c)
 {
-	*s = to;
+	int i = 0;
+
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+
+	if (s[i] == c)
+		return (&s[i]);
+
+	else
+		return (NULL);
 }
