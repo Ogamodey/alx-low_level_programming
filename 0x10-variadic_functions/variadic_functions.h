a2fcee7101f6b477683d1976ea21a2c74b95991d
ogamodey <ogamodeymoses@gmail.com>
2023-01-21 13:01:15 -0800

    Task0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,6 +9,4 @@ void print_all(const char * const format, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 
-_putchar
-
 #endif
